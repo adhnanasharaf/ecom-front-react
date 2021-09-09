56053afa5f6dd767350802ccee1c61e57671adac
Adhnan Asharaf <adhnanasharaf567@gmail.com>
2021-09-09 17:37:29 +0530

    make it better

--- src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(
-    createProxyMiddleware(["/api", , "/products"], { target: "https://nodejsserverecommerse.herokuapp.com" })
-  );
-};
