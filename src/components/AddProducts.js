1b883461ac7233e33656dd10667562f20e33eb42
Adhnan Asharaf <adhnanasharaf567@gmail.com>
2021-09-13 20:37:49 +0530

    better

--- src/components/AddProducts.js
+++ src/components/AddProducts.js
@@ -1,4 +1,4 @@
-import react, { Component } from "react";
+import React, { Component } from "react";
 import withContext from "../withContext";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
